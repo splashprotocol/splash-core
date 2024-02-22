41a8e074d257e0f529fce06bd96fb22faeb40273
Bromel777 <33941049+Bromel777@users.noreply.github.com>
2024-02-22 21:33:53 +0300

    DEV-277. Fix audit issues (#21)

--- plutarch-validators/WhalePoolsDex/PContracts/PAssets.hs
+++ plutarch-validators/WhalePoolsDex/PContracts/PAssets.hs
@@ -4,7 +4,7 @@ module WhalePoolsDex.PContracts.PAssets (
 ) where
 
 import WhalePoolsDex.PContracts.PApi (ownCurrencySymbol, tletUnwrap)
-import PExtra.API (assetClass, assetClassValueOf)
+import PExtra.API (assetClass, assetClassValueOf, pValueLength)
 import PExtra.List (pexists)
 import PExtra.Monadic
 import Plutarch
@@ -17,29 +17,31 @@ poolNftMintValidatorT oref tn = plam $ \_ ctx -> unTermCont $ do
     txinfo' <- tletField @"txInfo" ctx
     txinfo <- tcont $ pletFields @'["inputs", "mint"] txinfo'
     inputs <- tletUnwrap $ getField @"inputs" txinfo
+    valueMint <- tlet $ getField @"mint" txinfo
     let targetUtxoConsumed =
             let isTarget i = unTermCont $ do
                     let oref' = pfield @"outRef" # i
                     pure $ oref' #== oref
              in pexists # plam (isTarget) # inputs
         tokenMintExact = unTermCont $ do
-            valueMint <- tlet $ getField @"mint" txinfo
             let ownAc = assetClass # (ownCurrencySymbol # ctx) # tn
             pure $ assetClassValueOf # valueMint # ownAc #== 1
-    pure $ targetUtxoConsumed #&& tokenMintExact
+        correctTokensQty = (pValueLength # valueMint) #== 1
+    pure $ targetUtxoConsumed #&& tokenMintExact #&& correctTokensQty
 
 poolLqMintValidatorT :: Term s PTxOutRef -> Term s PTokenName -> Term s PInteger -> Term s (PData :--> PScriptContext :--> PBool)
 poolLqMintValidatorT oref tn emission = plam $ \_ ctx -> unTermCont $ do
     txinfo' <- tletField @"txInfo" ctx
     txinfo <- tcont $ pletFields @'["inputs", "mint"] txinfo'
     inputs <- tletUnwrap $ getField @"inputs" txinfo
+    valueMint <- tlet $ getField @"mint" txinfo
     let targetUtxoConsumed =
             let isTarget i = unTermCont $ do
                     oref' <- tletField @"outRef" i
                     pure $ oref' #== oref
              in pexists # plam (isTarget) # inputs
         tokenMintExact = unTermCont $ do
-            valueMint <- tlet $ getField @"mint" txinfo
             let ownAc = assetClass # (ownCurrencySymbol # ctx) # tn
             pure $ assetClassValueOf # valueMint # ownAc #== emission
-    pure $ targetUtxoConsumed #&& tokenMintExact
+        correctTokensQty = (pValueLength # valueMint) #== 1
+    pure $ targetUtxoConsumed #&& tokenMintExact #&& correctTokensQty
