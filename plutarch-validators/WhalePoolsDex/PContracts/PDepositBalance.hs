e53539efcb2d5a671271e4e8c5f86227222cfb31
Bromel777 <alexbromel@gmail.com>
2024-04-22 21:14:37 +0200

    Fix incorrect flow in deposit, redeem contracts

--- plutarch-validators/WhalePoolsDex/PContracts/PDepositBalance.hs
+++ plutarch-validators/WhalePoolsDex/PContracts/PDepositBalance.hs
@@ -57,92 +57,92 @@ depositValidatorT :: ClosedTerm (DepositBalanceConfig :--> OrderRedeemer :--> PS
 depositValidatorT = plam $ \conf' redeemer' ctx' -> unTermCont $ do
     ctx  <- pletFieldsC @'["txInfo", "purpose"] ctx'
     conf <- pletFieldsC @'["x", "y", "lq", "poolNft", "exFee", "rewardPkh", "stakePkh", "collateralAda"] conf'
+    redeemer <- pletFieldsC @'["poolInIx", "orderInIx", "rewardOutIx", "action"] redeemer'
     let 
         collateralAda = getField @"collateralAda" conf
 
         rewardPkh = getField @"rewardPkh" conf
-        stakePkh  = getField @"stakePkh"  conf
-        exFee     = getField @"exFee" conf
-
-        x  = getField @"x"  conf
-        y  = getField @"y"  conf
-        lq = getField @"lq" conf
-
-        txInfo' = getField @"txInfo" ctx
+        txInfo'   = getField @"txInfo" ctx
+        action = getField @"action" redeemer
     
     txInfo  <- pletFieldsC @'["inputs", "outputs", "signatories"] txInfo'
-    inputs  <- tletUnwrap $ getField @"inputs"  txInfo
-    outputs <- tletUnwrap $ getField @"outputs" txInfo
-
-    redeemer <- pletFieldsC @'["poolInIx", "orderInIx", "rewardOutIx", "action"] redeemer'
-    let 
-      poolInIx    = getField @"poolInIx"    redeemer
-      orderInIx   = getField @"orderInIx"   redeemer
-      rewardOutIx = getField @"rewardOutIx" redeemer
-      action = getField @"action" redeemer
-
-    rewardOut   <- tlet $ pelemAt # rewardOutIx # outputs
-    rewardValue <- tlet $ getRewardValue' # rewardOut # rewardPkh # stakePkh
-
-    poolIn'   <- tlet $ pelemAt # poolInIx # inputs
-    poolIn    <- pletFieldsC @'["outRef", "resolved"] poolIn'
-    let pool  = getField @"resolved" poolIn
-
-    poolValue <- tletField @"value" pool
-    let poolIdentity = -- operation is performed with the pool selected by the user 
-            let requiredNft = pfromData $ getField @"poolNft" conf
-                nftAmount = assetClassValueOf # poolValue # requiredNft
-             in nftAmount #== 1
     
-    poolInputDatum <- tlet $ extractBalancePoolConfig # pool
-    poolConf       <- pletFieldsC @'["treasuryX", "treasuryY"] poolInputDatum
-    let
-        treasuryX = getField @"treasuryX" poolConf
-        treasuryY = getField @"treasuryY" poolConf
-
-    selfIn'   <- tlet $ pelemAt # orderInIx # inputs
-    selfIn    <- pletFieldsC @'["outRef", "resolved"] selfIn'
-    selfValue <-
-        let self = pfromData $ getField @"resolved" selfIn
-         in tletField @"value" self
-
-    PSpending selfRef' <- pmatchC $ getField @"purpose" ctx
-    let 
-        selfIdentity =
-            let selfRef   = pfield @"_0" # selfRef'
-                selfInRef = getField @"outRef" selfIn
-             in selfRef #== selfInRef -- check that orderInIx points to the actual order
-
-        strictInputs = -- ensure double satisfaction attack is not possible
-            let inputsLength = plength # inputs
-             in inputsLength #== 2
-
-    liquidity <-
-        let lqNegative = assetClassValueOf # poolValue # lq
-         in tlet $ maxLqCap - lqNegative
-
-    reservesX <- tlet $ (assetClassValueOf # poolValue # x) - treasuryX
-    reservesY <- tlet $ (assetClassValueOf # poolValue # y) - treasuryY
-
-    minRewardByX <- tlet $ minAssetReward # selfValue # x # reservesX # liquidity # exFee # collateralAda
-    minRewardByY <- tlet $ minAssetReward # selfValue # y # reservesY # liquidity # exFee # collateralAda
-    let validChange = -- pair excess is returned to user
-            pif
-                (minRewardByX #== minRewardByY)
-                (pcon PTrue)
-                ( pif
-                    (minRewardByX #< minRewardByY)
-                    (validChange' # rewardValue # y # minRewardByY # minRewardByX # reservesY # liquidity)
-                    (validChange' # rewardValue # x # minRewardByX # minRewardByY # reservesX # liquidity)
-                )
-        minReward = pmin # minRewardByX # minRewardByY
-        validReward = -- calculated minimal output of LQ tokens is satisfied
-            let actualReward = assetClassValueOf # rewardValue # lq
-             in minReward #<= actualReward
-
     pure $
         pmatch action $ \case
-            Apply -> poolIdentity #&& selfIdentity #&& strictInputs #&& validChange #&& validReward
+            Apply -> unTermCont $ do
+                inputs  <- tletUnwrap $ getField @"inputs"  txInfo
+                outputs <- tletUnwrap $ getField @"outputs" txInfo
+                let
+                    poolInIx    = getField @"poolInIx"    redeemer
+                    orderInIx   = getField @"orderInIx"   redeemer
+                    rewardOutIx = getField @"rewardOutIx" redeemer
+
+                    stakePkh  = getField @"stakePkh"  conf
+                    exFee     = getField @"exFee" conf
+
+                    x  = getField @"x"  conf
+                    y  = getField @"y"  conf
+                    lq = getField @"lq" conf
+
+                rewardOut   <- tlet $ pelemAt # rewardOutIx # outputs
+                rewardValue <- tlet $ getRewardValue' # rewardOut # rewardPkh # stakePkh
+
+                poolIn'   <- tlet $ pelemAt # poolInIx # inputs
+                poolIn    <- pletFieldsC @'["outRef", "resolved"] poolIn'
+                let pool  = getField @"resolved" poolIn
+
+                poolValue <- tletField @"value" pool
+                let poolIdentity = -- operation is performed with the pool selected by the user 
+                        let requiredNft = pfromData $ getField @"poolNft" conf
+                            nftAmount = assetClassValueOf # poolValue # requiredNft
+                        in nftAmount #== 1
+                
+                poolInputDatum <- tlet $ extractBalancePoolConfig # pool
+                poolConf       <- pletFieldsC @'["treasuryX", "treasuryY"] poolInputDatum
+                let
+                    treasuryX = getField @"treasuryX" poolConf
+                    treasuryY = getField @"treasuryY" poolConf
+
+                selfIn'   <- tlet $ pelemAt # orderInIx # inputs
+                selfIn    <- pletFieldsC @'["outRef", "resolved"] selfIn'
+                selfValue <-
+                    let self = pfromData $ getField @"resolved" selfIn
+                    in tletField @"value" self
+
+                PSpending selfRef' <- pmatchC $ getField @"purpose" ctx
+                let 
+                    selfIdentity =
+                        let selfRef   = pfield @"_0" # selfRef'
+                            selfInRef = getField @"outRef" selfIn
+                        in selfRef #== selfInRef -- check that orderInIx points to the actual order
+
+                    strictInputs = -- ensure double satisfaction attack is not possible
+                        let inputsLength = plength # inputs
+                        in inputsLength #== 2
+
+                liquidity <-
+                    let lqNegative = assetClassValueOf # poolValue # lq
+                    in tlet $ maxLqCap - lqNegative
+
+                reservesX <- tlet $ (assetClassValueOf # poolValue # x) - treasuryX
+                reservesY <- tlet $ (assetClassValueOf # poolValue # y) - treasuryY
+
+                minRewardByX <- tlet $ minAssetReward # selfValue # x # reservesX # liquidity # exFee # collateralAda
+                minRewardByY <- tlet $ minAssetReward # selfValue # y # reservesY # liquidity # exFee # collateralAda
+                let validChange = -- pair excess is returned to user
+                        pif
+                            (minRewardByX #== minRewardByY)
+                            (pcon PTrue)
+                            ( pif
+                                (minRewardByX #< minRewardByY)
+                                (validChange' # rewardValue # y # minRewardByY # minRewardByX # reservesY # liquidity)
+                                (validChange' # rewardValue # x # minRewardByX # minRewardByY # reservesX # liquidity)
+                            )
+                    minReward = pmin # minRewardByX # minRewardByY
+                    validReward = -- calculated minimal output of LQ tokens is satisfied
+                        let actualReward = assetClassValueOf # rewardValue # lq
+                        in minReward #<= actualReward
+                pure $ poolIdentity #&& selfIdentity #&& strictInputs #&& validChange #&& validReward
             Refund ->
                 let sigs = pfromData $ getField @"signatories" txInfo
                  in containsSignature # sigs # rewardPkh -- user signed the refund
